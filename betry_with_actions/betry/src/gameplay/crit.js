e9a83a88851778283d8ce58be3aa5bc35dbd3f3e
arturmaster420 <arturmaster420@gmail.com>
2025-11-30 16:56:19 +0200

    Delete betry_with_actions/betry directory

--- betry_with_actions/betry/src/gameplay/crit.js
+++
@@ -1,7 +0,0 @@
-export function rollCrit(critChance) {
-  return Math.random() < critChance;
-}
-
-export function computeDamage(baseDamage, isCrit, critMult) {
-  return isCrit ? baseDamage * critMult : baseDamage;
-}
