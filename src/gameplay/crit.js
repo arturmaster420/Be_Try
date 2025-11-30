f5c67f0d60d0db79f63d14f7b6bae664ed4248fe
arturmaster420 <arturmaster420@gmail.com>
2025-11-30 23:10:16 +0200

    Add files via upload

---
+++ src/gameplay/crit.js
@@ -0,0 +1,7 @@
+export function rollCrit(chance) {
+  return Math.random() < chance;
+}
+
+export function computeDamage(baseDamage, isCrit, critMult) {
+  return isCrit ? baseDamage * critMult : baseDamage;
+}
